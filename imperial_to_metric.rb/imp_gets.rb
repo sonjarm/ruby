c59fa7c1e9515e2fc58460e28b7dcb72b98a20e6
Sonja R. Maddox <[sonjarmaddox@yahoo.com]>
2015-02-03 11:15:07 -0600

    this is a test run to check commits

---
+++ imperial_to_metric.rb/imp_gets.rb
@@ -0,0 +1,13 @@
+puts "Welcome! Hey what's your name?"
+my_name = gets
+
+height_feet = 5
+height_inches = 8
+height_inches = (12 * height_feet) + height_inches
+
+weight_pounds = 190
+
+height_centimeters = total_height_inches = 2.54
+weight_kilograms = weight_pounds * 0.453592
+
+puts "#{my_name.chomp} is #{height_centimeters}cm and #{weight_kilograms}kg."
